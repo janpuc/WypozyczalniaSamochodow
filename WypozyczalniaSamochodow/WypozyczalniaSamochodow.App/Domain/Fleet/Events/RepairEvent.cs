8f023364a688dc5eead1fb9c145433f81ae75835
janpuc <janpuc@proton.me>
2026-06-10 23:03:32 +0200

    🐛 Fix RepairEvent

--- WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Fleet/Events/RepairEvent.cs
+++ WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Fleet/Events/RepairEvent.cs
@@ -7,8 +7,13 @@ internal sealed class RepairEvent : VehicleEvent
 {
     public BrokenDownEvent Cause { get; }
 
-    public RepairEvent(DateRange period, BrokenDownEvent cause, string? description = null) : base(period, description) { }
-
+    public RepairEvent(DateRange period, BrokenDownEvent cause, string? description = null) : base(period, description)
+    {
+        if (period.To is null)
+            throw new DomainException("Naprawa musi mieć datę zakończenia.");
+        Cause = cause ?? throw new DomainException("Naprawa musi być powiązana z awarią.");
+    }
+    
     public override string Describe() => "Naprawa";
     public override T Accept<T>(IVehicleEventVisitor<T> visitor) => visitor.Visit(this);
 
