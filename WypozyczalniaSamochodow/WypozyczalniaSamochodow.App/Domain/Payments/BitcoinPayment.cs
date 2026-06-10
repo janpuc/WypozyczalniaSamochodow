e423bed07b50e0201df5cf2638263ccfa50ba312
Darren Stasiak <darren.stasiak@dev4you.pl>
2026-06-10 22:40:40 +0200

    added error handling implementation for payment related entities

--- WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Payments/BitcoinPayment.cs
+++ WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Payments/BitcoinPayment.cs
@@ -7,7 +7,9 @@ internal sealed class BitcoinPayment : Payment
     public string WalletAddress { get; }
     public BitcoinPayment(Money amount, DateTime paidAt, string walletAddress) : base(amount, paidAt)
     {
-
+        if (string.IsNullOrWhiteSpace(walletAddress))
+            throw new DomainException("Adres portfela nie może być pusty.");
+        WalletAddress = walletAddress.Trim();
     }
     public override string MethodName => "Bitcoin";
     public override string Describe() => $"Bitcoin ({WalletAddress})";
