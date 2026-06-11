b9b603d621c669032acad47edb92c7c610a40cdb
peccoth <peccoth101@gmail.com>
2026-06-11 20:36:40 +0200

    feat: implementing auth event and adding missing vehicle event constructors

--- WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Fleet/Events/MaintenanceEvent.cs
+++ WypozyczalniaSamochodow/WypozyczalniaSamochodow.App/Domain/Fleet/Events/MaintenanceEvent.cs
@@ -5,7 +5,10 @@ namespace WypozyczalniaSamochodow.App.Domain.Fleet.Events;
 
 internal sealed class MaintenanceEvent : VehicleEvent
 {
-    public MaintenanceEvent(DateRange period, string? description = null) : base(period, description) { }
+    public MaintenanceEvent(DateRange period, string? description = null) : base(period, description) { 
+        if (period.To is null)
+            throw new DomainException("Serwis musi mieć datę zakończenia.");
+    }
     public override string Describe() => "Serwis";
     public override T Accept<T>(IVehicleEventVisitor<T> visitor) => visitor.Visit(this);
 }
